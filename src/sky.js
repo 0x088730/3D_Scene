654c26bf1d2042c1c5e269ef816e4a10e6120f7f
0x088730 <royalsceptre88@gmail.com>
2023-09-18 22:03:09 -0700

    feat: sky

--- src/sky.js
+++ src/sky.js
@@ -20,9 +20,15 @@
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 	THE SOFTWARE.
 **/
+var firstEffect = [0.136, 0.16, 0.208, Math.floor(Math.random() * 10 + 1) / 10];
+var secondEffect = [0.115, 0.113, 0.134, Math.floor(Math.random() * 10 + 1) / 10];
+var thirdEffect = [0.141, 0.155, 0.168, Math.floor(Math.random() * 10 + 1) / 10];
+var fourthEffect = [0.218, 0.153, 0.113, Math.floor(Math.random() * 10 + 1) / 10];
+var fifthEffect = [0.33, 0.5, 1, Math.floor(Math.random() * 10 + 1) / 10];
+var sixthEffect = [0.221, 0.126, 0.094, Math.floor(Math.random() * 10 + 1) / 10];
+var seventhEffect = [0.147, 0.153, 0.151, Math.floor(Math.random() * 10 + 1) / 10];
 
-
-SkyDome = function(scene, gl) {
+SkyDome = function (scene, gl) {
 	this.scene = scene;
 	this.gl = gl;
 	this.datapath = '';
@@ -33,14 +39,8 @@ SkyDome = function(scene, gl) {
 	this.triangleCount = 0;
 	this.renderer = null;
 	this.skyTimer = 0;
-	const firstEffect = [0.136, 0.16, 0.208, Math.floor(Math.random() * 10 + 1) / 10];
-	const secondEffect = [0.115, 0.113, 0.134, Math.floor(Math.random() * 10 + 1) / 10];
-	const thirdEffect = [0.141, 0.155, 0.168, Math.floor(Math.random() * 10 + 1) / 10];
-	const fourthEffect = [0.218, 0.153, 0.113, Math.floor(Math.random() * 10 + 1) / 10];
-	const fifthEffect = [0.33, 0.5, 1, Math.floor(Math.random() * 10 + 1) / 10];
-	const sixthEffect = [0.221, 0.126, 0.094, Math.floor(Math.random() * 10 + 1) / 10];
-	const seventhEffect = [0.147, 0.153, 0.151, Math.floor(Math.random() * 10 + 1) / 10];
-	const optionsEffect = [firstEffect,secondEffect,thirdEffect,fourthEffect,fifthEffect,sixthEffect,seventhEffect]
+
+	const optionsEffect = [firstEffect, secondEffect, thirdEffect, fourthEffect, fifthEffect, sixthEffect, seventhEffect]
 	const randomIndexEffect = Math.floor(Math.random() * optionsEffect.length);
 	const randomValue = optionsEffect[randomIndexEffect];
 
@@ -48,13 +48,13 @@ SkyDome = function(scene, gl) {
 
 };
 
-SkyDome.prototype.onLoad = function(path) {
+SkyDome.prototype.onLoad = function (path) {
 	this.datapath = path + 'Sky/';
 	this.loadGroup('SkyGroup.xml');
 	this.loadShader('SkyShader.xml');
 };
 
-SkyDome.prototype.loadGroup = function(filename) {
+SkyDome.prototype.loadGroup = function (filename) {
 	var url = this.datapath + filename;
 
 	var xhr = new XMLHttpRequest;
@@ -63,55 +63,55 @@ SkyDome.prototype.loadGroup = function(filename) {
 
 	var self = this;
 
-	xhr.onreadystatechange = function() {
-		if(this.readyState == 4) {
-			if(this.status == 200 || this.status == 0) {
+	xhr.onreadystatechange = function () {
+		if (this.readyState == 4) {
+			if (this.status == 200 || this.status == 0) {
 				var xmldoc = this.responseXML;
 				// parse the sky dome group
 				var groupElem = xmldoc.firstChild;
-				if(!groupElem || groupElem.tagName != 'Group') {
+				if (!groupElem || groupElem.tagName != 'Group') {
 					err('invalid format: ' + url);
 					return;
 				}
 				var transformElem = groupElem.firstElementChild;
-				if(!transformElem || transformElem.tagName != 'Transform') {
+				if (!transformElem || transformElem.tagName != 'Transform') {
 					err('invalid format: ' + url);
 					return;
 				}
 				var transformChildElem = transformElem.firstElementChild;
-				while(transformChildElem) {
+				while (transformChildElem) {
 					var tuple3;
-					if(transformChildElem.hasAttribute('x') && transformChildElem.hasAttribute('y') && transformChildElem.hasAttribute('z')) {
-						tuple3 = [ 
-							parseFloat(transformChildElem.getAttribute('x')), 
-							parseFloat(transformChildElem.getAttribute('y')), 
+					if (transformChildElem.hasAttribute('x') && transformChildElem.hasAttribute('y') && transformChildElem.hasAttribute('z')) {
+						tuple3 = [
+							parseFloat(transformChildElem.getAttribute('x')),
+							parseFloat(transformChildElem.getAttribute('y')),
 							parseFloat(transformChildElem.getAttribute('z'))
 						];
 					}
-					switch(transformChildElem.tagName) {
-					case 'Translations':
-						if(tuple3)
-							self.transform.setTranslation(tuple3);
-						break;
-					case 'Rotations':
-						if(tuple3)
-							self.transform.setRotation(tuple3);
-						break;
-					case 'Scales':
-						if(tuple3)
-							self.transform.setScale(tuple3);
-						break;
-					default:
-						break;					
+					switch (transformChildElem.tagName) {
+						case 'Translations':
+							if (tuple3)
+								self.transform.setTranslation(tuple3);
+							break;
+						case 'Rotations':
+							if (tuple3)
+								self.transform.setRotation(tuple3);
+							break;
+						case 'Scales':
+							if (tuple3)
+								self.transform.setScale(tuple3);
+							break;
+						default:
+							break;
 					}
 					transformChildElem = transformChildElem.nextElementSibling;
 				}
 				var shapeElem = transformElem.nextElementSibling;
-				if(!shapeElem || shapeElem.tagName != 'Shape') {
+				if (!shapeElem || shapeElem.tagName != 'Shape') {
 					err('invalid format: ' + url);
 					return;
 				}
-				if(!shapeElem.hasAttribute('path')) {
+				if (!shapeElem.hasAttribute('path')) {
 					err('shape is not defined: ' + url);
 					return;
 				}
@@ -121,14 +121,14 @@ SkyDome.prototype.loadGroup = function(filename) {
 		}
 	};
 
-	xhr.onerror = function() {
+	xhr.onerror = function () {
 		err('failed to load ' + url);
 	};
 
 	xhr.send();
 };
 
-SkyDome.prototype.loadShader = function(filename) {
+SkyDome.prototype.loadShader = function (filename) {
 	var url = this.datapath + filename;
 
 	var xhr = new XMLHttpRequest;
@@ -137,46 +137,46 @@ SkyDome.prototype.loadShader = function(filename) {
 
 	var self = this;
 
-	xhr.onreadystatechange = function() {
-		if(this.readyState == 4) {
-			if(this.status == 200 || this.status == 0) {
+	xhr.onreadystatechange = function () {
+		if (this.readyState == 4) {
+			if (this.status == 200 || this.status == 0) {
 				var xmldoc = this.responseXML;
 				// parse the sky dome shader
 				var shadersElem = xmldoc.firstChild;
-				if(!shadersElem || shadersElem.tagName != 'Shaders') {
+				if (!shadersElem || shadersElem.tagName != 'Shaders') {
 					err('invalid format: ' + url);
 					return;
 				}
 				var vs, ps;
 				var vertexShaderElem = shadersElem.firstElementChild;
-				if(!vertexShaderElem || vertexShaderElem.tagName != 'VERTEX_SHADER') {
+				if (!vertexShaderElem || vertexShaderElem.tagName != 'VERTEX_SHADER') {
 					err('vertex shader is not defined: ' + url);
 					return;
 				}
 				var vertexShaderChildElem = vertexShaderElem.firstElementChild;
-				while(vertexShaderChildElem) {
-					if(vertexShaderChildElem.tagName == 'RawData') {
+				while (vertexShaderChildElem) {
+					if (vertexShaderChildElem.tagName == 'RawData') {
 						vs = vertexShaderChildElem.textContent;
 						break;
 					}
 					vertexShaderChildElem = vertexShaderChildElem.nextElementSibling;
 				}
 				var pixelShaderElem = vertexShaderElem.nextElementSibling;
-				if(!pixelShaderElem || pixelShaderElem.tagName != 'PIXEL_SHADER') {
+				if (!pixelShaderElem || pixelShaderElem.tagName != 'PIXEL_SHADER') {
 					err('pixel shader is not defined: ' + url);
 					return;
 				}
 				var pixelShaderChildElem = pixelShaderElem.firstElementChild;
-				while(pixelShaderChildElem) {
-					if(pixelShaderChildElem.tagName == 'RawData') {
+				while (pixelShaderChildElem) {
+					if (pixelShaderChildElem.tagName == 'RawData') {
 						ps = pixelShaderChildElem.textContent;
 						break;
 					}
 					pixelShaderChildElem = pixelShaderChildElem.nextElementSibling;
 				};
-				if(vs && ps) {
+				if (vs && ps) {
 					var program = new SglProgram(self.gl, [vs], [ps]);
-					if(!program.isValid) {
+					if (!program.isValid) {
 						err('failed to create program: ' + url);
 						err(program.log);
 						return;
@@ -188,14 +188,14 @@ SkyDome.prototype.loadShader = function(filename) {
 		}
 	};
 
-	xhr.onerror = function() {
+	xhr.onerror = function () {
 		err('failed to load ' + url);
 	};
 
 	xhr.send();
 };
 
-SkyDome.prototype.loadShape = function(filename) {
+SkyDome.prototype.loadShape = function (filename) {
 	var url = this.datapath + filename;
 
 	var xhr = new XMLHttpRequest;
@@ -204,29 +204,29 @@ SkyDome.prototype.loadShape = function(filename) {
 
 	var self = this;
 
-	xhr.onreadystatechange = function() {
-		if(this.readyState == 4) {
-			if(this.status == 200 || this.status == 0) {
+	xhr.onreadystatechange = function () {
+		if (this.readyState == 4) {
+			if (this.status == 200 || this.status == 0) {
 				var xmldoc = this.responseXML;
 				// parse the sky dome shape
 				var shapeElem = xmldoc.firstChild;
-				if(!shapeElem || shapeElem.tagName != 'Shape') {
+				if (!shapeElem || shapeElem.tagName != 'Shape') {
 					err('invalid format: ' + url);
 					return;
 				}
 				// parse appearance
 				var appearanceElem = shapeElem.firstElementChild;
-				if(!appearanceElem || appearanceElem.tagName != 'Appearance') {
+				if (!appearanceElem || appearanceElem.tagName != 'Appearance') {
 					err('invalid format: ' + url);
 					return;
 				}
 				// parse texture 
 				var textureElem = appearanceElem.firstElementChild;
-				if(!textureElem || textureElem.tagName != 'Texture') {
+				if (!textureElem || textureElem.tagName != 'Texture') {
 					err('texture is not defined: ' + url);
 					return;
 				}
-				if(textureElem.hasAttribute('path')) {
+				if (textureElem.hasAttribute('path')) {
 					var path = '';
 					var mipmap = false;
 					var wrapS = self.gl.CLAMP_TO_EDGE;
@@ -236,75 +236,75 @@ SkyDome.prototype.loadShape = function(filename) {
 					path = textureElem.getAttribute('path');
 					mipmap = (textureElem.getAttribute('mipmap') == 'true') ? true : false;
 					var textureChildElem = textureElem.firstElementChild;
-					while(textureChildElem) {
-						switch(textureChildElem.tagName) {
-						case 'Wrap':
-							wrapS = (textureChildElem.getAttribute('s') == 'REPEAT') ? self.gl.REPEAT : self.gl.CLAMP_TO_EDGE;
-							wrapT = (textureChildElem.getAttribute('t') == 'REPEAT') ? self.gl.REPEAT : self.gl.CLAMP_TO_EDGE;
-							break;
-						case 'Filter':
-							minFilter = (textureChildElem.getAttribute('min') == 'LINEAR_MIPMAP_LINEAR') ? (mipmap ? self.gl.LINEAR_MIPMAP_LINEAR : self.gl.LINEAR) : self.gl.NEAREST;
-							magFilter = (textureChildElem.getAttribute('mag') == 'LINEAR') ? self.gl.LINEAR : self.gl.NEAREST;
-							break;
-						default:
-							break;
+					while (textureChildElem) {
+						switch (textureChildElem.tagName) {
+							case 'Wrap':
+								wrapS = (textureChildElem.getAttribute('s') == 'REPEAT') ? self.gl.REPEAT : self.gl.CLAMP_TO_EDGE;
+								wrapT = (textureChildElem.getAttribute('t') == 'REPEAT') ? self.gl.REPEAT : self.gl.CLAMP_TO_EDGE;
+								break;
+							case 'Filter':
+								minFilter = (textureChildElem.getAttribute('min') == 'LINEAR_MIPMAP_LINEAR') ? (mipmap ? self.gl.LINEAR_MIPMAP_LINEAR : self.gl.LINEAR) : self.gl.NEAREST;
+								magFilter = (textureChildElem.getAttribute('mag') == 'LINEAR') ? self.gl.LINEAR : self.gl.NEAREST;
+								break;
+							default:
+								break;
 						}
 						textureChildElem = textureChildElem.nextElementSibling;
 					}
-					if(path != '') {
+					if (path != '') {
 						var opt = {
-							minFilter:			minFilter, 
-							magFilter:			magFilter, 
-							wrapS:				wrapS, 
-							wrapT:				wrapT, 
-							generateMipmap:		mipmap
+							minFilter: minFilter,
+							magFilter: magFilter,
+							wrapS: wrapS,
+							wrapT: wrapT,
+							generateMipmap: mipmap
 						};
 						self.texture = new SglTexture2D(self.gl, self.datapath + path, opt);
 					}
 				}
 				// parse geometry
 				var geometryElem = appearanceElem.nextElementSibling;
-				if(!geometryElem || geometryElem.tagName != 'Geometry') {
+				if (!geometryElem || geometryElem.tagName != 'Geometry') {
 					err('geometry is not defined: ' + url);
 					return;
 				}
 				var vertices, normals, indices;
 				var geometryChildElem = geometryElem.firstElementChild;
-				while(geometryChildElem) {
+				while (geometryChildElem) {
 					var rawDataElem = geometryChildElem.firstElementChild;
-					if(rawDataElem && rawDataElem.tagName == 'RawData') {
+					if (rawDataElem && rawDataElem.tagName == 'RawData') {
 						var data = rawDataElem.textContent.split(' ');
-						if(data.length > 0) {
-							switch(geometryChildElem.tagName) {
-							case 'TexCoords':
-								// ignore texture coords, for these will be generated at runtime by the vertex shader
-								break;
-							case 'Vertices':
-								vertices = new Array(data.length);
-								for(var i=0; i<data.length; i++) {
-									vertices[i] = parseFloat(data[i]);
-								}
-								break;
-							case 'Normals':
-								normals = new Array(data.length);
-								for(var i=0; i<data.length; i++) {
-									normals[i] = parseFloat(data[i]);
-								}
-								break;
-							case 'Indices':
-								indices = new Array(data.length);
-								for(var i=0; i<data.length; i++) {
-									indices[i] = parseInt(data[i]);
-								}
-								break;
-							default:
-								break;
+						if (data.length > 0) {
+							switch (geometryChildElem.tagName) {
+								case 'TexCoords':
+									// ignore texture coords, for these will be generated at runtime by the vertex shader
+									break;
+								case 'Vertices':
+									vertices = new Array(data.length);
+									for (var i = 0; i < data.length; i++) {
+										vertices[i] = parseFloat(data[i]);
+									}
+									break;
+								case 'Normals':
+									normals = new Array(data.length);
+									for (var i = 0; i < data.length; i++) {
+										normals[i] = parseFloat(data[i]);
+									}
+									break;
+								case 'Indices':
+									indices = new Array(data.length);
+									for (var i = 0; i < data.length; i++) {
+										indices[i] = parseInt(data[i]);
+									}
+									break;
+								default:
+									break;
 							}
 						}
 					}
 					geometryChildElem = geometryChildElem.nextElementSibling;
 				}
-				if(vertices && indices) {
+				if (vertices && indices) {
 					var meshJS = new SglMeshJS();
 					meshJS.addVertexAttribute('position', 3, vertices);
 					meshJS.addIndexedPrimitives('triangles', SGL_TRIANGLES_LIST, indices);
@@ -317,19 +317,19 @@ SkyDome.prototype.loadShape = function(filename) {
 		}
 	};
 
-	xhr.onerror = function() {
+	xhr.onerror = function () {
 		err('failed to load ' + url);
 	};
 
 	xhr.send();
 };
 
-SkyDome.prototype.update = function(deltaTime) {
+SkyDome.prototype.update = function (deltaTime) {
 	this.skyTimer += 0.0125 * this.skyInfo[3] * deltaTime;
 };
 
-SkyDome.prototype.render = function(camera) {
-	if(this.isReady()) {
+SkyDome.prototype.render = function (camera) {
+	if (this.isReady()) {
 		var w = this.scene.getApp().getWidth();
 		var h = this.scene.getApp().getHeight();
 
@@ -366,7 +366,7 @@ SkyDome.prototype.render = function(camera) {
 	return 0;
 };
 
-SkyDome.prototype.isReady = function() {
-	return this.renderer && this.mesh && 
-			this.texture && this.texture.isValid;
+SkyDome.prototype.isReady = function () {
+	return this.renderer && this.mesh &&
+		this.texture && this.texture.isValid;
 };
